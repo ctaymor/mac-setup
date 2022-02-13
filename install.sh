7f45e6045b47b099ec4ac37a073e2feaeb06014e
Caroline Taymor <ctaymor@gmail.com>
2022-02-13 15:30:59 -0800

    Install Brew when needed

--- install.sh
+++ install.sh
@@ -8,6 +8,12 @@ function run_subscripts {
 	done
 }
 function main {
+	which brew
+	if [[ $? -ne 0 ]]; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/ctaymor/.zprofile
+	fi
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 	echo "Running brew bundle"
 	brew bundle
 	echo "Running ./scripts/*"
