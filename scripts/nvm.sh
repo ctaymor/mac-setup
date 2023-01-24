47a33e5857538465e771a136da8391af8d2fe6db
Caroline Taymor <ctaymor@gmail.com>
2023-01-23 21:08:43 -0800

    fix nvm

--- scripts/nvm.sh
+++ scripts/nvm.sh
@@ -6,10 +6,8 @@ set -eux
 # install or update
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
-ln -s ~/workspace/mac-setup/dotfiles/nvim/user/plugins.lua ~/.config/nvim/lua/user/plugins.lua 
 # Set the current shell to have access to nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvim +PackerSync
