8ca39d4376baa8d64085011798f932d4eb6cb56b
Caroline Taymor <ctaymor@gmail.com>
2023-02-23 17:05:22 -0800

    Switch out luanvim for a lightweight config inspired by https://www.youtube.com/watch?v=J9yqSdvAKXY

---
+++ dotfiles/nvim/lua/keymaps.lua
@@ -0,0 +1,9 @@
+vim.opt.cursorline = true
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+
+-- use spaces for tabs
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
---
+++ dotfiles/nvim/lua/plugin-config/init.lua
@@ -0,0 +1,5 @@
+require("plugin-config.nvim-tree")
+require("plugin-config.lualine")
+require("plugin-config.tokyonight")
+require("plugin-config.telescope")
+require("plugin-config.treesitter")
