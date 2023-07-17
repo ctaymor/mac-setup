06d8e070b07911d40c9eb531989cc5c292a7dd60
Caroline Taymor <ctaymor@gmail.com>
2023-07-17 15:05:27 -0700

    Add kubectl-for-linux script

---
+++ scripts/kubectl-for-linux.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+set -eux
+
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl apt-transport-https
+
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update
+sudo apt-get install -y kubectl
+
