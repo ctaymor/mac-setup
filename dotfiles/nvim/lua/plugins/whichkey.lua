5c2fefe167a8e1756760fe3f90ad63da783c53c0
Caroline Taymor <ctaymor@gmail.com>
2024-08-06 21:20:53 -0700

    Add which-key plugin for nvim to dotfiles
    A plugin [recommended by Nat
    Bennett](https://neovim.substack.com/p/one-great-plugin-which-key) which
    helps you learn keybindings for nvim

---
+++ dotfiles/nvim/lua/plugins/whichkey.lua
@@ -0,0 +1,5 @@
+-- Which-key is a tool to help you learn and remember vim keybindings, both custom and default.
+-- If you type the beginning of a command, it will show the options you can type next in a pop up menu
+return {
+	"folke/which-key.nvim"
+}
