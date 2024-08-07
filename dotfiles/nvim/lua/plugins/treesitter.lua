b0edc39756be17d50b9cdd427eccbab3532744c6
Caroline Taymor <ctaymor@gmail.com>
2024-08-06 21:27:50 -0700

    Add treesitter plugin for nvim to dotfiles
    I had this previously. It provides better parsing for various languages
    in nvim, and is used by telescope and other tools.

---
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,23 @@
+-- Treesitter provides better language parsing for nvim
+return {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  }
+
