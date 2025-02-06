0dce884a79b199efed65eb85d2e0721e3406d6ae
Caroline Taymor <ctaymor@gmail.com>
2025-02-06 11:56:04 -0800

    Simplify Vim setup
    Stop using the Lazy neovim distro, and only use it as a plugin manager.
    Use basic whichkey and color syntax plugins

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -1,1 +1,53 @@
-require("config.lazy")
+-- syntax highlighting
+-- tabs v spaces
+-- tree finder
+-- color scheme
+
+--- bootstrap plugin manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+--- load plugins
+local plugins = {
+  { "folke/tokyonight.nvim" },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  }
+}
+require("lazy").setup(plugins)
+
+
+--- somehow, this stops Nvim from adding a tab
+--- when it autoindents a line
+--- in addition to the spaces its copying from the above line
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+--- set line numbers for pairing
+vim.opt.number = true
+
+--- enable 24-bit RGB colors
+vim.opt.termguicolors = true
+
+--- set the colorscheme
+vim.cmd("colorscheme tokyonight")
